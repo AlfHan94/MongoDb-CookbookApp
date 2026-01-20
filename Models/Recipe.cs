de354cdf32ec9f74b8dddaa2f66f0dabd1ff0699
AlfHan94 <alfredhandin@gmail.com>
2026-01-20 19:00:14 +0100

    Built project structure and added Recipe and Review Models

---
+++ Models/Recipe.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CookbookApp.Models;
+
+public class Recipe
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string? Description { get; set; }
+
+    public List<string> Ingredients { get; set; } = new();
+
+    public List<string> Steps { get; set; } = new();
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
---
+++ Models/Review.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CookbookApp.Models;
+
+public class Review
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string RecipeId { get; set; } = string.Empty;
+
+    public string? Comment { get; set; }
+
+    public int Rating { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
