8b8d1ed3a5c2d2a17905514b1e74a19ded38b38e
AlfHan94 <alfredhandin@gmail.com>
2026-01-21 06:58:14 +0100

    Added Create, Read and Update functionalty for RecipeService

---
+++ Services/RecipeService.cs
@@ -0,0 +1,34 @@
+﻿using CookbookApp.Data;
+using CookbookApp.Models;
+using MongoDB.Driver;
+
+namespace CookbookApp.Services;
+
+public class RecipeService
+{
+    private readonly IMongoCollection<Recipe> _recipes;
+
+    public RecipeService(MongoContext context)
+    {
+        _recipes = context.Recipes;
+    }
+
+    public void AddRecipe(Recipe recipe)
+    {
+        _recipes.InsertOne(recipe);
+    }
+    public List<Recipe> GetAllRecipes()
+    {
+        return _recipes.Find(_ => true).ToList();
+    }
+
+    public void UpdateRecipe(string id, Recipe updateRecipe)
+    {
+        var filter = Builders<Recipe>.Filter.Eq(r => r.Id, id);
+
+        _recipes.ReplaceOne(filter, updateRecipe);
+    }
+
+
+
+}
