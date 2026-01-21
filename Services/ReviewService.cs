0f1c688d425a1db190edbb87d6f6720f42ccd583
AlfHan94 <alfredhandin@gmail.com>
2026-01-21 19:29:47 +0100

    ADd ReviewService with create and list by recipe

--- Services/ReviewService.cs
+++ Services/ReviewService.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CookbookApp.Data;
+using CookbookApp.Models;
+using MongoDB.Driver;
 
-namespace CookbookApp.Services
+
+namespace CookbookApp.Services;
+
+public class ReviewService
 {
-    internal class ReviewService
+    private readonly IMongoCollection<Review> _reviews;
+
+    public ReviewService(MongoContext context)
     {
+        _reviews = context.Reviews;
     }
+
+    public void AddReview(Review review)
+    {
+        _reviews.InsertOne(review);
+    }
+
+    public List<Review> GetReviewsForRecipe(string recipeId)
+    {
+        var filter = Builders<Review>.Filter.Eq(r => r.RecipeId, recipeId);
+        return _reviews.Find(filter).ToList();
+    }
+
 }
