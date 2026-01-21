ca881604cd2f3b4d8d13d6bb2559a88cd7ab4b18
AlfHan94 <alfredhandin@gmail.com>
2026-01-21 06:02:47 +0100

    Add MongoContext for MongoDB connection

---
+++ Data/MongoContext.cs
@@ -0,0 +1,28 @@
+﻿using CookbookApp.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookbookApp.Data;
+
+
+public class MongoContext
+{
+
+    private readonly IMongoDatabase _database;
+
+    public MongoContext()
+    {
+        var client = new MongoClient("mongodb://localhost:27017/");
+        _database = client.GetDatabase("AlfredHandin");
+    }
+
+    public IMongoCollection<Recipe> Recipes =>
+        _database.GetCollection<Recipe>("recipes");
+    public IMongoCollection<Review> Reviews =>
+        _database.GetCollection<Review>("reviews");
+
+}
